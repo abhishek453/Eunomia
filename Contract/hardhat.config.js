c5a0e4a5b9f8b1b24c0657b69d29ed98da64f837
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2021-10-24 12:46:05 +0530

    Update hardhat.config.js

--- Contract/hardhat.config.js
+++ Contract/hardhat.config.js
@@ -20,8 +20,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/G3_UvKtg98d9tS2Exvn-20heXkxdXVAR`,
-      accounts: [`c4e0d488744f1eefb91f2fd9a19021e79b8cb177ccce53bd7f31602285443e9d`]
+      url: `RANDOM-STUFF`,
+      accounts: [`RANDOM-STUFF`]
     }
   }
 };
